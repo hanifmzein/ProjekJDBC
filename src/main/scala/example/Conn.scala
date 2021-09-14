2670fed50bb38629aabd8c8bb6692f472533a3f9
Muhamad Hanif Zein <hanifmzein@gmail.com>
2021-09-14 10:10:57 +0700

    sudah_jadi

--- src/main/scala/example/Conn.scala
+++ src/main/scala/example/Conn.scala
@@ -1,21 +1,21 @@
 package example
 
-import java.net.URL
+import java.io.FileInputStream
 import java.sql.Connection
 import java.util.Properties
-import scala.io.Source
-import java.io.FileInputStream
 
 class Conn {
     var connection:Connection = _
-    val properties = new Properties()
-    val in = new FileInputStream("application.properties")
-    properties.load(in)
+
+    val input = new FileInputStream("config.properties")
+    val properties = new Properties();
+
+    // load a properties file
+    properties.load(input);
 
     val host: String = properties.getProperty("host")
     val driver: String = properties.getProperty("driver")
     val username: String = properties.getProperty("username")
     val password: String = properties.getProperty("password")
-
 
 }
